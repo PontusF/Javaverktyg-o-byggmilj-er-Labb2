365b1ede5fc073b1ed3bad62d5fd793caad1d45a
Pontus <Pontus.fredriksson1@gmail.com>
2018-11-27 16:25:30 +0100

    Finish Task 5

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,6 +1,16 @@
 public class GuessingGame {
     public static void main(String[] args) {
-        Guesser guesser = new Guesser(1,1000);
+        int low = 0;
+        int high = 1000;
+        try{
+            low = Integer.parseInt(args[0]);
+            high = Integer.parseInt(args[1]);
+
+        }catch (Exception e){
+
+        }
+
+        Guesser guesser = new Guesser(low,high);
         guesser.start();
     }
 }
